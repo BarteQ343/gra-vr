3e1a1f9f0f2c0c20fff0d2d1e0b0ac4eb194e96e
BarteQ <bartoszchmiel72@gmail.com>
2025-02-12 23:41:30 +0100

    Dodane dźwięki otwierania i kilka animacji

--- Assets/Script/Opener.cs
+++ Assets/Script/Opener.cs
@@ -7,33 +7,44 @@ public class Opener : MonoBehaviour
 	public Unlocker keyToDoor;
 	public Animator anim;
 	public bool unlockedByDefault = false;
+	public AudioSource audioSourceUnlocked;
+    public AudioSource audioSourceLocked;
 
-	public void OpenDoor()
+    public void OpenDoor()
 	{
 		if (keyToDoor == null && unlockedByDefault == true)
-		{
-            if (anim.GetBool("open") == false)
-            {
-                print("opening");
-                anim.SetBool("open", true);
-            }
-            else
-            {
-                print("closing");
-                anim.SetBool("open", false);
-            }
-        } else if (keyToDoor.unlocked == true)
 		{
 			if (anim.GetBool("open") == false)
 			{
 				print("opening");
 				anim.SetBool("open", true);
+				audioSourceUnlocked.Play();
 			}
 			else
 			{
 				print("closing");
 				anim.SetBool("open", false);
+                audioSourceUnlocked.Play();
 			}
+		}
+		else if (keyToDoor.unlocked == true)
+		{
+			if (anim.GetBool("open") == false)
+			{
+				print("opening");
+				anim.SetBool("open", true);
+                audioSourceUnlocked.Play();
+			}
+			else
+			{
+				print("closing");
+				anim.SetBool("open", false);
+                audioSourceUnlocked.Play();
+			}
+		}
+		else if (keyToDoor.unlocked == false)
+		{
+			audioSourceLocked.Play();
 		}
 	}
 }
--- Assets/Script/Stash/StashTriggerArea.cs
+++ Assets/Script/Stash/StashTriggerArea.cs
@@ -5,13 +5,16 @@ namespace Script.Stash
 {
     public class StashTriggerArea : MonoBehaviour
     {
+        public Animator anim;
+        public AudioSource audioSourceUnlocked;
         [SerializeField] private GameObject stashPanel;
         
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Magnet"))
             {
-                Destroy(stashPanel);
+                anim.SetBool("open", true);
+                audioSourceUnlocked.Play();
             }
         }
     }
