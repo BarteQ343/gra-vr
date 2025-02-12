f625fc5be5c02a27e9763c2bb933214ef9633c9e
GlebIssayev <84099997+N0vago@users.noreply.github.com>
2025-02-12 19:28:59 +0100

    stash

---
+++ Assets/Script/Stash/StashTriggerArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+
+namespace Script.Stash
+{
+    public class StashTriggerArea : MonoBehaviour
+    {
+        [SerializeField] private GameObject stashPanel;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Magnet"))
+            {
+                Destroy(stashPanel);
+            }
+        }
+    }
+}
